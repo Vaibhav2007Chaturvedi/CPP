63a2cc500fe28128cd243b831734167312d39b94
VAIBHAV CHATURVEDI <vaibhav0090v@gmail.com>
2026-04-08 15:51:27 +0000

    problem solving

--- Function/problem1.cpp
+++ Function/problem1.cpp
@@ -15,16 +15,37 @@
 
 // Write a program if a number is odd or even:
 
+// #include<iostream>
+// using namespace std;
+// int number(int n){
+//     if(n%2==0){
+//         return 1;
+//     }
+//     else{
+//         return 0;
+//     }
+// }
+// int main(){
+//     cout<<number(2);
+// }
+
+// .............................
+// Write a program to print factoriall of a number ,n:
 #include<iostream>
 using namespace std;
-int number(int n){
-    if(n%2==0){
-        return 1;
+int factorial(int n){
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact=fact*i;
     }
-    else{
-        return 0;
-    }
+    cout<<n<<"="<<fact<<endl;
+    return fact;
 }
 int main(){
-    cout<<number(2);
+    factorial(0);
+    factorial(1);
+    factorial(2);
+    factorial(3);
+    factorial(4);
+return 0;
 }
