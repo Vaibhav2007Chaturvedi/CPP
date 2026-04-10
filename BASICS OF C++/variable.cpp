a127f28215c429bb4c0a3189e5ee2e96ec0e56e8
VAIBHAV CHATURVEDI <vaibhav0090v@gmail.com>
2026-04-10 16:13:37 +0000

    Variable in c++

---
+++ BASICS OF C++/variable.cpp
@@ -0,0 +1,18 @@
+// The "places" in which we store data are called objects . To access an object , we need name .
+// A named object is called a variable and has a specific type (such as int or string).
+
+#include<iostream>
+using namespace std;
+int main(){
+    int number_step=39; //int for integer
+    double flying_time=3.5; //double for floating-point numbber
+    char decimal_point='.'; //char for individual character
+    string name="VAIBHAV"; //strinng for character strings
+    bool tap=true; //bool for logical variable
+    cout << number_step << " " 
+         << flying_time << " " 
+         << decimal_point << " " 
+         << name << " " 
+         << tap << endl;
+
+}
